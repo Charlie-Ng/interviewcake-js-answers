14147514a681603eb21cd36dc7c2d4ef6358a443
Charlie-Ng <kcng@ucdavis.edu>
2016-05-06 00:37:08 -0700

    self-1-find-the-longest-palindrome.js

---
+++ self-1-find-the-longest-palindrome.js
@@ -0,0 +1,39 @@
+// find the longest palindrome with a given string
+
+function findPalindrome(str) {
+
+	if(str === null) return null;
+	var palindrome = "";
+	var maxPalindrome = "";
+
+	var len = str.length;
+
+	for(var i = 0; i < len; i++) {
+
+		var temp_i = i;
+		var jIndex;
+		for(var j = len - 1; j >= i; j--) {
+
+			if(str[temp_i] === str[j]) {
+
+				temp_i++;
+
+				if(j === i) {
+
+					palindrome = str.substring(i, temp_i);
+				}
+
+			}else {
+				temp_i = i;
+			}
+		}
+		if(palindrome.length > maxPalindrome.length) {
+			maxPalindrome = palindrome;
+		}	
+	}
+	return maxPalindrome;
+}
+
+console.log(findPalindrome("b112a211aaaaccccccccbbbbbbdcccccccaaaa311sdfsdfsdfkldsflllld"));//cccccccc
+console.log(findPalindrome("11cbbc11c1"));//11cbbc11
+console.log(findPalindrome("asdfasdfasdfasdfasdffdasasdfddkkddfdsafdafdafdafdasdfasdfsdafasdfasdf")); //asdfddkkddfdsa
