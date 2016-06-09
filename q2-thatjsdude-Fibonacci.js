eff6d85eda3c32f765e8aa3c33a6900e551ca526
Charlie-Ng <kcng@ucdavis.edu>
2016-06-09 14:22:15 -0700

    q2-thatjsdude.js

--- q2-thatjsdude-Fibonacci.js
+++ q2-thatjsdude-Fibonacci.js
@@ -1,25 +1,5 @@
 //How do get nth Fibonacci number?
 
-function findFibonacci(n) {
-
-	var fib = [1 , 1]; //or [0, 1]
-
-	if(n <= 2) return 1; // or 1
-
-	for(var i = 2; i < n; i++) {
-
-		fib[i] = fib[i - 1] + fib[i - 2];
-	}
-
-	return fib[n - 1];
-}
-
-console.log(findFibonacci(12)); //144
-console.log(findFibonacci(3)); //2
-console.log(findFibonacci(5)); //5
-//O(n)
-console.log("done part 1");
-
 // Interviewer: can you make it recursive?
 
 function findFib(n) {
@@ -34,5 +14,27 @@ function findFib(n) {
 console.log(findFib(12)); //144
 console.log(findFib(3)); //2
 console.log(findFib(5)); //5
-
 //O(2^n)
+console.log("done part 1");
+
+// use DB
+function findFibDB(n) {
+
+	var table = {};
+	table[0] = 0;
+	table[1] = 1;
+
+	for(var i = 2; i <= n; i++) {
+		table[i] = table[i - 1] + table[i - 2]; 
+	}
+
+	return table[n];
+}
+
+console.log(findFibDB(12));
+console.log(findFibDB(3));
+console.log(findFibDB(5));
+//O(n) 
+console.log("done part 2");
+
+
